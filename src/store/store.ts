24dbbc593211cced16186e06ff8fd7a90bfb1e7c
mariusz-pacyga-wttech <mariusz.pacyga@wundermanthompson.com>
2022-12-30 11:49:23 +0100

    installments count refactoring

--- src/store/store.ts
+++ src/store/store.ts
@@ -4,6 +4,7 @@ import {
   Installment,
   OptionsObj,
   Overpayment,
+  OverpaymentDate,
   UserInputs,
 } from '../components/types';
 import { generateDatesArray } from '../Utils/generateDatesArray';
@@ -11,6 +12,8 @@ import { InitialValues } from '../Utils/initialValues';
 import { countInstallment, odsetki } from '../Utils/Helpers';
 import { clearStorageData, saveDataToStorage } from '../Utils/dataFromStorage';
 import { Message, messages } from './messages';
+import { isEmpty, isNil } from 'ramda';
+import { overpaymentsReduce } from '../Utils/overpaymentsReduce';
 
 export default class Store {
   userInputs: UserInputs;
@@ -31,6 +34,16 @@ export default class Store {
     this.showBanner = false;
     this.message = messages.saveStorage;
     this.error = false;
+  }
+
+  get overpaymentDates(): OverpaymentDate[] {
+    return this.overpayments.reduce<OverpaymentDate[]>((acc, curr) => {
+      if (curr.repeatPeriod !== undefined) {
+        return [...acc];
+      } else {
+        return [...acc, { date: curr.date, value: curr.value }];
+      }
+    }, []);
   }
 
   public setOptions(options: Partial<OptionsObj>): void {
@@ -109,51 +122,51 @@ export default class Store {
   }
 
   public get installments(): Installment[] {
-    let amountLeft: number = this.userInputs.amount.value;
-    let overpaymentsLeft = this.overpayments;
-    const result: Installment[] = [];
+    const gross = this.userInputs.wibor.value + this.userInputs.bankgross.value;
 
-    for (
-      let index = 0;
-      index < this.userInputs.period.value && amountLeft > 0;
-      index++
-    ) {
-      const rata: number = countInstallment(
-        amountLeft,
-        this.userInputs.wibor.value + this.userInputs.bankgross.value,
-        this.userInputs.period.value - index
-      );
-      amountLeft =
-        amountLeft -
-        rata +
-        odsetki(
-          amountLeft,
-          this.userInputs.bankgross.value + this.userInputs.wibor.value
+    const result = this.dates.reduce<{
+      amountLeft: number;
+      installments: Installment[];
+    }>(
+      (acc, curr, index) => {
+        const installment = countInstallment(
+          acc.amountLeft,
+          gross,
+          this.userInputs.period.value - index
         );
-      while (
-        overpaymentsLeft.length > 0 &&
-        overpaymentsLeft[0].date < this.dates[index]
-      ) {
-        amountLeft = amountLeft - overpaymentsLeft[0].value;
-        overpaymentsLeft = overpaymentsLeft.slice(1, overpaymentsLeft.length);
-      }
 
-      if (this.options.constRateOverpayment) {
-        amountLeft = amountLeft - this.options.constRateOverpaymentValue + rata;
-      }
-      result.push({ value: rata, date: this.dates[index] });
-    }
-    return result;
+        const amountLeft = this.options.constRateOverpayment
+          ? acc.amountLeft - this.options.constRateOverpaymentValue
+          : acc.amountLeft - installment + odsetki(acc.amountLeft, gross);
+
+        const overpayments = this.overpaymentDates.filter((overpayment) => {
+          if (isNil(acc.installments.at(-1)?.date)) {
+            return overpayment.date < curr;
+          }
+          return (
+            overpayment.date > acc.installments.at(-1).date &&
+            overpayment.date < curr
+          );
+        });
+
+        return {
+          installments: [
+            ...acc.installments,
+            { date: curr, value: installment },
+          ],
+          amountLeft: isEmpty(overpayments)
+            ? amountLeft
+            : amountLeft - overpaymentsReduce(overpayments),
+        };
+      },
+      { amountLeft: this.userInputs.amount.value, installments: [] }
+    );
+
+    return result.installments.filter((installment) => installment.value > 0);
   }
 
-  public get overpaymentsTotal(): number {
-    if (this.overpayments.length === 0) return 0;
-    return this.overpayments.reduce<{ value: number; date: Date }>(
-      (prev, current) => {
-        return { value: current.value + prev.value, date: new Date() };
-      },
-      { value: 0, date: new Date() }
-    ).value;
+  get overpaymentsTotal(): number {
+    return overpaymentsReduce(this.overpaymentDates);
   }
 
   public setUserInput(key: InputNames, value: number): void {
